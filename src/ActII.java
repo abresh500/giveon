e7a1744dc0efc152f99cf5666e583a142455e805
abenezer nebiyu <abenezernebiyu500@gmail.com>
2026-05-20 12:01:14 +0300

    GOAT

---
+++ src/ActII.java
@@ -0,0 +1,22 @@
+public class ActII {
+
+    void play(){
+        System.out.println("Jezebel is playing");
+        System.out.println("tonight u and i we shared ur love time to time another man will have whats mine from time to time its no surprise and i dont mind it pretty jezebel mineeee");
+    }
+
+    void play2(){
+        System.out.println("Replica is playing");
+        System.out.println("guess i should let go tried but u are special all this time passin by me and i can't even get next to you so i got replica");
+    }
+
+    void play3(){
+        System.out.println("Fool Me Once is playing");
+        System.out.println("i said fool me once shame on me fool me twice maybe i'll allow it baby i like you and there is no other way around it");
+    }
+
+    void play4(){
+        System.out.println("Save Me is playing");
+        System.out.println("u can always count on me want you and all in betweens i'll be the one you come and run to are you gon save for me");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ActII album = new ActII();
+
+        System.out.println("Choose a song:");
+        System.out.println("1. Jezebel");
+        System.out.println("2. Replica");
+        System.out.println("3. Fool Me Once");
+        System.out.println("4. Save Me");
+
+        int choice = input.nextInt();
+
+        switch (choice) {
+            case 1:
+                album.play();
+                break;
+            case 2:
+                album.play2();
+                break;
+            case 3:
+                album.play3();
+                break;
+            case 4:
+                album.play4();
+                break;
+            default:
+                System.out.println("Invalid choice");
+        }
+
+        input.close();
+    }
+}
